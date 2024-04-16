631c0496df0bc8cce9e7752d01a7e33be351e4e9
rania3103 <raniaabassi33@gmail.com>
2024-04-16 13:01:08 +0100

    try to fix it to handle all cases

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -4,19 +4,21 @@
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """returns a new matrix if possible otherwise none"""
-    if not mat1 or not mat2:
-        return None
-    if len(mat1) != len(mat2):
-        return None
     new_mat = []
     if axis == 0:
-        new_mat += mat1
-        new_mat += mat2
-        return new_mat
+        if len(mat1[0]) != len(mat2[0]):
+            return None
+        else:
+            new_mat += mat1
+            new_mat += mat2
+            return new_mat
     elif axis == 1:
         new_row = []
-        for row in range(len(mat1)):
-            temp = mat1[row] + mat2[row]
-            new_row.append(temp)
-        new_mat += new_row
-        return new_mat
+        if len(mat1) != len(mat2):
+            return None
+        else:
+            for row in range(len(mat1)):
+                temp = mat1[row] + mat2[row]
+                new_row.append(temp)
+            new_mat += new_row
+            return new_mat
