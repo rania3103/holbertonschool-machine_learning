04e2bda8fb59f92eff13b79aa7556986440167f0
rania3103 <raniaabassi33@gmail.com>
2024-04-16 16:06:50 +0100

    without importing module

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python3
 """a function that performs element-wise addition,
 subtraction, multiplication, and division"""
-import numpy as np
 
 
 def np_elementwise(mat1, mat2):
     """returns a tuple containing the element-wise sum,
     difference, product, and quotient, respectively"""
-    return (
-        np.add(
-            mat1, mat2), np.subtract(
-            mat1, mat2), np.multiply(
-                mat1, mat2), np.divide(
-                    mat1, mat2))
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
