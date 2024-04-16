dba1ba8200cb78341d5bdabdaa8c2c90ef20da76
rania3103 <raniaabassi33@gmail.com>
2024-04-16 12:34:00 +0100

    Gettin’ Cozy

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""a function that concatenates two matrices along a specific axis"""
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """returns a new matrix if possible otherwise none"""
+    new_mat = []
+    if axis == 0:
+        new_mat += mat1
+        new_mat += mat2
+        return new_mat
+    elif axis == 1:
+        new_row = []
+        for row in range(len(mat1)):
+            temp = mat1[row] + mat2[row]
+            new_row.append(temp)
+        new_mat += new_row
+        return new_mat
+    else:
+        return None
