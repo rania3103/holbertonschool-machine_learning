710f8135d93aa3816e92ee35f141a1fbb0b7254a
rania3103 <raniaabassi33@gmail.com>
2024-04-16 10:03:29 +0100

    Line Up

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""a function that adds two arrays element-wise"""
+
+
+def add_arrays(arr1, arr2):
+    """returns a new list"""
+    if len(arr1) != len(arr2):
+        return None
+    else:
+        new_arr = []
+        mat = []
+        mat += [arr1]
+        mat += [arr2]
+        for col in range(len(mat) * 2):
+            sum_col = 0
+            for row in range(2):
+                sum_col += mat[row][col]
+            new_arr.append(sum_col)
+        return new_arr
