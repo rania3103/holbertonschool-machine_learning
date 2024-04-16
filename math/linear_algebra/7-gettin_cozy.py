5483338b58760f5a6672ce9cec21316afdc62154
rania3103 <raniaabassi33@gmail.com>
2024-04-16 12:44:21 +0100

    handle the case when dimensions don't match

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -4,17 +4,18 @@
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """returns a new matrix if possible otherwise none"""
-    new_mat = []
-    if axis == 0:
-        new_mat += mat1
-        new_mat += mat2
-        return new_mat
-    elif axis == 1:
-        new_row = []
-        for row in range(len(mat1)):
-            temp = mat1[row] + mat2[row]
-            new_row.append(temp)
-        new_mat += new_row
-        return new_mat
-    else:
+    if not mat1 or not mat2 or len(mat1) != len(mat2):
         return None
+    else:
+        new_mat = []
+        if axis == 0:
+            new_mat += mat1
+            new_mat += mat2
+            return new_mat
+        elif axis == 1:
+            new_row = []
+            for row in range(len(mat1)):
+                temp = mat1[row] + mat2[row]
+                new_row.append(temp)
+            new_mat += new_row
+            return new_mat
