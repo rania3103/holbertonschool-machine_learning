c04ce0a9d8c0fd7021fa15eabe1f6f71563a7ebb
rania3103 <raniaabassi33@gmail.com>
2024-04-16 11:20:47 +0100

    Howdy Partner

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" a function that concatenates two arrays"""
+
+
+def cat_arrays(arr1, arr2):
+    """returns a new list"""
+    new_arr = []
+    new_arr += arr1
+    new_arr += arr2
+    return new_arr
