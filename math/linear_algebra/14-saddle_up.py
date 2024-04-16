539d46cceafd33e4cdefaec058c5a5b2ae315f9b
rania3103 <raniaabassi33@gmail.com>
2024-04-16 16:25:22 +0100

    Saddle Up

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" a function that performs matrix multiplication"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """returns the result of the multiplication of 2 matrices"""
+    return np.dot(mat1, mat2)
