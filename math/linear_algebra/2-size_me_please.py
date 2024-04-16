c20f2bce8653c328b86393d6095dbef1a4380cdc
rania3103 <raniaabassi33@gmail.com>
2024-04-16 07:15:36 +0100

    Size Me Please

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""calculates the shape of a matrix"""
+
+
+def matrix_shape(matrix):
+    """returns a list of integers"""
+    if matrix:
+        numb_rows = len(matrix)
+        numb_cols = len(matrix[0])
+        if isinstance(matrix[0][0], list):
+            numb_items = len(matrix[0][0])
+            return [numb_rows, numb_cols, numb_items]
+        else:
+            return [numb_rows, numb_cols]
+    else:
+        return []
