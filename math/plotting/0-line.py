d2543a01bb941b3373b9f86d2337d2f8c6b22514
rania3103 <raniaabassi33@gmail.com>
2024-04-17 10:21:09 +0100

    fix x axis limit

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
+"""code to plot y as a line graph"""
 
 
 def line():
-
+    """y is plotted as a solid red line"""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-    x = np.arange(0, 11)
-    plt.plot(x, y, '-r')
+    plt.plot(y, '-r')
+    plt.xlim(0, 10)
     plt.show()
