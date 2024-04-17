96cde8f79debcf349d9c2163d48fa01c72aac771
rania3103 <raniaabassi33@gmail.com>
2024-04-17 11:56:59 +0100

    Frequency

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""code to plot a histogram of student scores for a project"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """ x-axis has bins every 10 units"""
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+    plt.hist(student_grades, 10, (0, 100), edgecolor='black')
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+    plt.title('Project A')
+    plt.show()
