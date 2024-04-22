cbd708ea3c36c7d27db3d141462733e17db7d981
rania3103 <raniaabassi33@gmail.com>
2024-04-22 14:22:00 +0100

    try to fix the case when poly is [0]

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -9,12 +9,15 @@ def poly_integral(poly, C=0):
         return None
     else:
         integral_coef = [C]
-        integral_coef.append(poly[0])
-        for i in range(1, len(poly)):
-            if poly[i] == 0:
-                integral_coef.append(0)
-            elif poly[i] % (i + 1) != 0:
-                integral_coef.append(poly[i] / (i + 1))
-            else:
-                integral_coef.append(poly[i] // (i + 1))
-        return integral_coef
+        if all(poly[i] == 0 for i in range(1, len(poly))):
+            return integral_coef
+        else:
+            integral_coef.append(poly[0])
+            for i in range(1, len(poly)):
+                if poly[i] == 0:
+                    integral_coef.append(0)
+                elif poly[i] % (i + 1) != 0:
+                    integral_coef.append(poly[i] / (i + 1))
+                else:
+                    integral_coef.append(poly[i] // (i + 1))
+            return integral_coef
