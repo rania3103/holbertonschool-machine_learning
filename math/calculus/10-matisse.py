4096fab03cd11071fa628775f9119371d954ebab
rania3103 <raniaabassi33@gmail.com>
2024-04-22 13:19:39 +0100

    Derive happiness in oneself from a good day's work

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""a function that calculates the derivative of a polynomial"""
+
+
+def poly_derivative(poly):
+    """Returns a new list of coefficients representing
+    the derivative of the polynomial"""
+    if not poly:
+        return None
+    else:
+        derv_coef = poly[1:]
+        for i in range(1, len(derv_coef)):
+            if derv_coef[i] != 0:
+                derv_coef[i] *= i + 1
+        return derv_coef
