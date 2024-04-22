9b46796960e9b100aff8be39d6458cc38dd446ea
rania3103 <raniaabassi33@gmail.com>
2024-04-22 12:23:17 +0100

    Our life is the sum total of all the decisions we make every day

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""a function that calculates the sum of i^2 from i =1 to n"""
+
+
+def summation_i_squared(n):
+    """Return the integer value of the sum"""
+    if not isinstance(n, int):
+        return None
+    else:
+        return (n * (n + 1) * (2 * n + 1)) // 6
