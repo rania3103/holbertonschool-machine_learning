9fccfff85f96dbd863f7b0126a437fd51886dfc2
rania3103 <raniaabassi33@gmail.com>
2024-04-25 19:15:50 +0100

    try to fix output

--- supervised_learning/decision_tree/2-build_decision_tree.py
+++ supervised_learning/decision_tree/2-build_decision_tree.py
@@ -78,14 +78,10 @@ class Node:
     def right_child_add_prefix(self, text):
         """add prefix to the right child"""
         lines = text.split("\n")
-        if str(self.right_child).split(' ')[0] == 'node' or str(
-                self.left_child).split(' ')[0] == 'node':
-            new_text = "    +--->" + lines[0] + "\n"
-        else:
-            new_text = "    +--" + lines[0] + "\n"
-        for x in lines[1:-1]:
+        new_text = "    +--" + lines[0] + "\n"
+        for x in lines[1:]:
             new_text += ("      " + x) + "\n"
-        return (new_text)
+        return (new_text.rstrip())
 
     def __str__(self):
         """string representation of node"""
