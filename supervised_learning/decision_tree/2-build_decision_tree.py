2ab1fed06f72979092999bf69863fb17bcdab547
rania3103 <raniaabassi33@gmail.com>
2024-04-26 15:35:00 +0100

    try to fix the output

--- supervised_learning/decision_tree/2-build_decision_tree.py
+++ supervised_learning/decision_tree/2-build_decision_tree.py
@@ -82,7 +82,7 @@ class Node:
         #         self.left_child).split(' ')[0] == 'node':
         new_text = "    +--" + lines[0]
         for x in lines[1:]:
-            new_text += "\n      " + x
+            new_text += "\n       " + x
         return (new_text)
 
     def __str__(self):
