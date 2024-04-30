a670f6f2495fb9f028b515bd7ef28249b34c0028
rania3103 <raniaabassi33@gmail.com>
2024-04-30 13:29:39 +0100

    Neuron Forward Propagation

---
+++ supervised_learning/classification/2-neuron.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""Write a class Neuron that defines a single
+neuron performing binary classification"""
+import numpy as np
+
+
+class Neuron:
+    """defines a single neuron performing binary classification"""
+
+    def __init__(self, nx):
+        """nx is the number of input features to the neuron,
+        W: The weights vector for the neuron,
+        b: The bias for the neuron,
+        A: The activated output of the neuron"""
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self.__W = np.random.randn(1, nx)
+        self.__b, self.__A = 0, 0
+
+    @property
+    def W(self):
+        """returns The weights vector for the neuron"""
+        return self.__W
+
+    @property
+    def b(self):
+        """returns The bias for the neuron."""
+        return self.__b
+
+    @property
+    def A(self):
+        """returns The activated output of the neuron."""
+        return self.__A
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neuron"""
+        self.__A = 1 / (1 + np.exp((np.matmul(self.__W, X) + self.__b) * -1))
+        return self.__A
