dd6f4bb2b12c57de0754421513525fc80a7e1fba
rania3103 <raniaabassi33@gmail.com>
2024-05-15 17:36:09 +0100

    Accuracy

---
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Write a function that calculates the accuracy of a prediction"""
+import tensorflow.compat.v1 as tf
+
+
+def calculate_accuracy(y, y_pred):
+    """Returns: a tensor containing the decimal accuracy of the prediction"""
+    acc = tf.metrics.accuracy(
+        tf.argmax(
+            y, axis=1), tf.argmax(
+            y_pred, axis=1), tf.shape(y_pred)[1])[0]
+    return tf.metrics.mean(acc)[0]
