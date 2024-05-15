6d723e0002203f2c57bd7c82b9b58fe433957aa0
rania3103 <raniaabassi33@gmail.com>
2024-05-15 19:36:55 +0100

    fix it

--- supervised_learning/optimization/0-norm_constants.py
+++ supervised_learning/optimization/0-norm_constants.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-import numpy as np
 """Write a function that normalizes (standardizes) a matrix:"""
+import numpy as np
 
 
 def normalization_constants(X):
     """Returns: The normalized X matrix"""
-    return np.mean(X, axis=0), np.mean(X, axis=0)
+    return np.mean(X, axis=0), np.std(X, axis=0)
