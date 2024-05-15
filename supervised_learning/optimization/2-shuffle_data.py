94e4d478aeae2ad6a8b77662a3a180c29f41ca33
rania3103 <raniaabassi33@gmail.com>
2024-05-15 19:54:35 +0100

    fix pep8

--- supervised_learning/optimization/2-shuffle_data.py
+++ supervised_learning/optimization/2-shuffle_data.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import numpy as np
-"""Write a function that shuffles the data points in two matrices the same way"""
+"""Write a function that shuffles the data
+points in two matrices the same way"""
 
 
 def shuffle_data(X, Y):
