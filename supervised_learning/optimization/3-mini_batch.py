c6f540e5ae275e72eaae16a1d7eb247d7ed9500f
rania3103 <raniaabassi33@gmail.com>
2024-05-16 13:12:39 +0100

    try to handle mini batch

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -14,8 +14,11 @@ def create_mini_batches(X, Y, batch_size):
     batches = []
     for i in range(num_batches):
         stard_ind = i * batch_size
-        end_ind = min(stard_ind + batch_size, len(X))
+        end_ind = stard_ind + batch_size
         batch_X = shuf_X[stard_ind:end_ind]
         batch_Y = shuf_Y[stard_ind:end_ind]
+        if len(X) % batch_size != 0:
+            batch_X = shuf_X[stard_ind:]
+            batch_Y = shuf_Y[stard_ind:]
         batches.append((batch_X, batch_Y))
     return batches
