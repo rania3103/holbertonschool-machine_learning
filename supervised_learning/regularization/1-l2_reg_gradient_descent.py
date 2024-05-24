32d5eb0ee8ae70974864ab9cdb43af946c996fe5
rania3103 <raniaabassi33@gmail.com>
2024-05-24 18:26:52 +0100

    fix pycodestyle

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -9,17 +9,14 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """Returns: the updated weights and
     biases of a neural network"""
     m = Y.shape[1]
-    for lay in range(1, L + 1):
-        a = cache['A' + str(lay)]
-        if lay == L:
-            y = Y.reshape(a.shape)
-        a -= y
-        prev_a = cache['A' + str(lay - 1)]
+    for lay in range(L):
+        a = cache['A' + str(lay + 1)] - Y
+        prev_a = cache['A' + str(lay)]
         d_w = np.dot(a, prev_a.T) / m
-        w = weights['W' + str(lay)]
+        w = weights['W' + str(lay + 1)]
         reg_term = lambtha / (2 * m)
         d_w += reg_term * w
         d_b = np.sum(a, axis=1) / m
-        weights['b' + str(lay)] -= alpha * d_b
-        weights['W' + str(lay)] -= alpha * d_w
+        weights['b' + str(lay + 1)] -= alpha * d_b
+        weights['W' + str(lay + 1)] -= alpha * d_w
     return weights
