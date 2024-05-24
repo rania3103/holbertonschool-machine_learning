9027be3000ce64e9f16efbaa77519f2671538cf5
rania3103 <raniaabassi33@gmail.com>
2024-05-24 12:22:07 +0100

    update it to float

--- supervised_learning/error_analysis/0-create_confusion.py
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -8,9 +8,9 @@ def create_confusion_matrix(labels, logits):
     with row indices representing the correct labels
     and column indices representing the predicted labels"""
     classes = labels.shape[1]
-    con_mat = np.zeros((classes, classes), dtype=int)
+    con_mat = np.zeros((classes, classes), dtype=float)
     max_ind_list_labels = np.argmax(labels, axis=1)
     max_ind_list_logits = np.argmax(logits, axis=1)
-    for i, j in zip(max_ind_list_labels, max_ind_list_logits):
-        con_mat[i, j] += 1
+    for i in range(len(max_ind_list_labels)):
+        con_mat[max_ind_list_labels[i], max_ind_list_logits[i]] += 1
     return con_mat
