fbe735babcbb37733301c17025eadeab3132f96e
rania3103 <raniaabassi33@gmail.com>
2024-05-24 14:59:33 +0100

    remove com

--- supervised_learning/error_analysis/1-sensitivity.py
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -7,8 +7,6 @@ import numpy as np
 def sensitivity(confusion):
     """Returns: a numpy.ndarray of shape (classes,)
     containing the sensitivity of each class"""
-    # classes = confusion.shape[1]
-    # sen = np.ndarray(classes,)
     true_pos = np.diag(confusion)
     false_neg = np.sum(confusion, axis=1) - true_pos
     return true_pos / (false_neg + true_pos)
