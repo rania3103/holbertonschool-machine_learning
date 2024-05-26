3651645dc2c5bdc674695bb010ff1e328c97e399
rania3103 <raniaabassi33@gmail.com>
2024-05-26 19:58:50 +0100

    try to fix it using back prop

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -8,15 +8,22 @@ import numpy as np
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """Returns: the updated weights and
     biases of a neural network"""
+
+    A_prev = cache['A0']
     m = Y.shape[1]
-    for lay in range(L):
-        a = cache['A' + str(lay + 1)]
-        prev_a = cache['A' + str(lay)]
-        d_w = np.dot(a, prev_a.T) / m
-        w = weights['W' + str(lay + 1)]
-        reg_term = lambtha / m * w
-        d_w += reg_term
-        d_b = np.sum(a, axis=1, keepdims=True) / m
-        weights['b' + str(lay + 1)] -= alpha * d_b
-        weights['W' + str(lay + 1)] -= alpha * d_w
+    for lay in range(L, 0, -1):
+        A = cache['A' + str(lay)]
+        if lay == L:
+            der = A - Y
+        else:
+            der = d_A * (1 - A ** 2)
+        if lay > 1:
+            d_A = np.dot(weights['W' + str(lay)].T, der)
+        A_prev = cache['A' + str(lay - 1)]
+        reg_term = (lambtha / m)
+        d_W = np.dot(der, A_prev.T) + reg_term * weights['W' + str(lay)]
+        d_b = np.sum(der, axis=1, keepdims=True) / m
+
+        weights['W' + str(lay)] -= alpha * d_W
+        weights['b' + str(lay)] -= alpha * d_b
     return weights
