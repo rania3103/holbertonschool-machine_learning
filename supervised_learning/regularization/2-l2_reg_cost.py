81906127d033bcdcfbbee8dfa135daaf5c4b34c4
rania3103 <raniaabassi33@gmail.com>
2024-05-26 15:31:32 +0100

    try to fix it

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -8,7 +8,7 @@ def l2_reg_cost(cost, model):
     """Returns: a tensor containing the total
     cost for each layer of the network,
     accounting for L2 regularization"""
-    reg_loss = tf.add_n(model.losses)
+    reg_loss = tf.reduce_sum(model.losses)
     if not isinstance(cost, tf.Tensor):
         cost = tf.convert_to_tensor(cost, dtype=tf.float32)
     return cost + reg_loss
