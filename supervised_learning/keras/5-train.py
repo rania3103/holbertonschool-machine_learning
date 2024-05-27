3b5e8ab221dde0819f7383520909dd88a3e6dfea
rania3103 <raniaabassi33@gmail.com>
2024-05-27 09:59:20 +0100

    Validate

---
+++ supervised_learning/keras/5-train.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""a function that trains a model using mini-batch gradient descent
+and to also analyze validaiton data"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, verbose=True, shuffle=False):
+    """validation_data is the data to validate the model with, if not None"""
+    return network.fit(data, labels, batch_size, epochs,
+                       validation_data, verbose, shuffle)
