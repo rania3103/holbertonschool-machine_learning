f3971b06514d185c2b84ad607cdb2bf10ae81d29
rania3103 <raniaabassi33@gmail.com>
2024-05-27 10:05:27 +0100

    fix it

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -7,5 +7,6 @@ import tensorflow.keras as K
 def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, verbose=True, shuffle=False):
     """validation_data is the data to validate the model with, if not None"""
-    return network.fit(data, labels, batch_size, epochs,
-                       validation_data, verbose, shuffle)
+    return network.fit(data, labels, batch_size=batch_size,
+                       epochs=epochs, validation_data=validation_data,
+                       verbose=verbose, shuffle=shuffle)
