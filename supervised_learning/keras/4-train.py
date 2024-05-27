140b5f4b03d82c3678369535f116663e6f083c34
rania3103 <raniaabassi33@gmail.com>
2024-05-27 10:03:27 +0100

    fix it

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -6,4 +6,5 @@ import tensorflow.keras as K
 def train_model(network, data, labels, batch_size, epochs,
                 verbose=True, shuffle=False):
     """Returns: the History object generated after training the model"""
-    return network.fit(data, labels, batch_size, epochs, verbose, shuffle)
+    return network.fit(data, labels, batch_size=batch_size,
+                       epochs=epochs, verbose=verbose, shuffle=shuffle)
