6884a8b124459e6ecd5fc389ce8678b8f5aa59a9
rania3103 <raniaabassi33@gmail.com>
2024-05-27 10:54:41 +0100

    Early Stopping

--- supervised_learning/keras/6-train.py
+++ supervised_learning/keras/6-train.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 """a function that trains a model using mini-batch gradient descent
-and to also analyze validaiton data"""
+and also analyzes validaiton data and trains the model using early stopping"""
 import tensorflow.keras as K
 
 
 def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, early_stopping=False, patience=0,
                 verbose=True, shuffle=False):
-    """validation_data is the data to validate the model with, if not None"""
+    """early_stopping is a boolean that indicates whether
+    early stopping should be used
+    patience is the patience used for early stopping
+    """
     if early_stopping and validation_data:
         callback = K.callbacks.EarlyStopping(monitor="val_loss",
                                              patience=patience)
