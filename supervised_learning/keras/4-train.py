d6111dbd3c791a2e50eae79c4ed6a8cc568d0385
rania3103 <raniaabassi33@gmail.com>
2024-05-27 09:53:24 +0100

    train

---
+++ supervised_learning/keras/4-train.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""a function that trains a model using mini-batch gradient descent"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                verbose=True, shuffle=False):
+    """Returns: the History object generated after training the model"""
+    return network.fit(data, labels, batch_size, epochs, verbose, shuffle)
