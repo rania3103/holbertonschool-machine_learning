39c03c61fe37521024cbd85ab25861b3b5996a53
rania3103 <raniaabassi33@gmail.com>
2024-05-29 17:50:40 +0100

    try to fix it

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -11,11 +11,10 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     if padding == 'valid':
         ph, pw = 0, 0
     elif padding == 'same':
-        ph = ((h - 1) * sh + kh - h) // 2
-        pw = ((w - 1) * sw + kw - w) // 2
+        ph = (((h - 1) * sh + kh - h) // 2) + 1
+        pw = (((w - 1) * sw + kw - w) // 2) + 1
 
-    pad_img = np.pad(images, ((0, 0), (ph, ph), (pw, pw)),
-                     mode='constant', constant_values=0)
+    pad_img = np.pad(images, ((0, 0), (ph, ph), (pw, pw)))
 
     out_h = ((h + 2 * ph - kh) // sh) + 1
     out_w = ((w + 2 * pw - kw) // sw) + 1
