7713fb738831b7d7426d16f9822b47d62d8e6b01
rania3103 <raniaabassi33@gmail.com>
2024-05-29 17:57:14 +0100

    handle the case when padding is tuple

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -13,7 +13,7 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     elif padding == 'same':
         ph = (((h - 1) * sh + kh - h) // 2) + 1
         pw = (((w - 1) * sw + kw - w) // 2) + 1
-    elif isinstance(padding, set):
+    elif isinstance(padding, tuple):
         ph, pw = padding
 
     pad_img = np.pad(images, ((0, 0), (ph, ph), (pw, pw)))
