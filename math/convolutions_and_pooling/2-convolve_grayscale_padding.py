56d0b35d7ac0ab0f7c31a5ad0291654d9de11241
rania3103 <raniaabassi33@gmail.com>
2024-05-29 16:26:28 +0100

    fix output dimensions

--- math/convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -8,12 +8,17 @@ def convolve_grayscale_padding(images, kernel, padding):
     """Returns: a numpy.ndarray containing the convolved images"""
     m, h, w = images.shape
     kh, kw = kernel.shape
+
     ph, pw = padding
     pad_img = np.pad(
         images, ((0, 0), (ph, ph), (pw, pw)))
-    output = np.zeros((m, h, w))
-    for i in range(h):
-        for j in range(w):
+
+    out_h = h + 2 * ph - kh + 1
+    out_w = w + 2 * pw - kw + 1
+
+    output = np.zeros((m, out_h, out_w))
+    for i in range(out_h):
+        for j in range(out_w):
             output[:, i, j] = np.sum(
                 pad_img[:, i:i + kh, j:j + kw] * kernel, axis=(1, 2))
     return output
