325dff04b28594758efc48d715551840ab622239
rania3103 <raniaabassi33@gmail.com>
2024-05-29 15:48:27 +0100

    Convolution with Padding

---
+++ math/convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""a function that performs a same convolution on
+grayscale images with custom padding"""
+import numpy as np
+
+
+def convolve_grayscale_padding(images, kernel, padding):
+    """Returns: a numpy.ndarray containing the convolved images"""
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+    ph, pw = padding
+    pad_img = np.pad(
+        images, ((0, 0), (ph, ph), (pw, pw)))
+    output = np.zeros((m, h, w))
+    for i in range(h):
+        for j in range(w):
+            output[:, i, j] = np.sum(
+                pad_img[:, i:i + kh, j:j + kw] * kernel, axis=(1, 2))
+    return output
