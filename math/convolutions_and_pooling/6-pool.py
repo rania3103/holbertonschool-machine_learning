110a004153035fe3f8f6b165f006f15c6d4bb5fa
rania3103 <raniaabassi33@gmail.com>
2024-05-30 07:44:39 +0100

    fix pycodestyle

--- math/convolutions_and_pooling/6-pool.py
+++ math/convolutions_and_pooling/6-pool.py
@@ -9,19 +9,19 @@ def pool(images, kernel_shape, stride, mode='max'):
     kh, kw = kernel_shape
     sh, sw = stride
 
-    out_h = ((h  - kh) // sh) + 1
-    out_w = ((w  - kw) // sw) + 1
+    out_h = ((h - kh) // sh) + 1
+    out_w = ((w - kw) // sw) + 1
 
     output = np.zeros((m, out_h, out_w, c))
 
     for i in range(out_h):
         for j in range(out_w):
             if mode == 'max':
-                    output[:, i, j, :] = np.max(
-                        images[:, i * sh:i * sh + kh, j * sw:j * sw + kw, :]
-                        , axis=(1, 2))
+                output[:, i, j, :] = np.max(
+                    images[:, i * sh:i * sh + kh, j * sw:j * sw + kw, :],
+                    axis=(1, 2))
             elif mode == 'avg':
                 output[:, i, j, :] = np.mean(
-                        images[:, i * sh:i * sh + kh, j * sw:j * sw + kw, :]
-                        , axis=(1, 2))
+                    images[:, i * sh:i * sh + kh, j * sw:j * sw + kw, :],
+                    axis=(1, 2))
     return output
