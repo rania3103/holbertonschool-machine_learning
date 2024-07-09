81990ac572d3097946e8ea503c4df36a9af67e40
rania3103 <raniaabassi33@gmail.com>
2024-07-09 11:53:22 +0100

    add staircase

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -7,4 +7,5 @@ import tensorflow as tf
 def learning_rate_decay(alpha, decay_rate, decay_step):
     """Returns: the learning rate decay operation"""
     return tf.keras.optimizers.schedules.InverseTimeDecay(
-        initial_learning_rate=alpha, decay_steps=decay_step, decay_rate=decay_rate)
+        initial_learning_rate=alpha, decay_steps=decay_step,
+        decay_rate=decay_rate, staircase=True)
