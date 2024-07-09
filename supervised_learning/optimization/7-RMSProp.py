8b13934b6002fcfd197dd3d3ffb10be8e729e300
rania3103 <raniaabassi33@gmail.com>
2024-07-09 11:18:06 +0100

    fix it

--- supervised_learning/optimization/7-RMSProp.py
+++ supervised_learning/optimization/7-RMSProp.py
@@ -6,6 +6,6 @@ import numpy as np
 
 def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
     """Returns: the updated variable and the new moment, respectively"""
-    s *= beta2 + (1 - beta2) * np.square(grad)
-    var -= alpha * grad / (np.sqrt(s) + epsilon)
+    s = beta2 * s + (1 - beta2) * np.square(grad)
+    var = var - alpha * grad / (np.sqrt(s) + epsilon)
     return var, s
