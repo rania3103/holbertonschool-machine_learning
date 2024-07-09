8b496dfab8c0fcece040c6d84fd4bb1282c64834
rania3103 <raniaabassi33@gmail.com>
2024-07-09 11:38:53 +0100

    fix it

--- supervised_learning/optimization/9-Adam.py
+++ supervised_learning/optimization/9-Adam.py
@@ -7,7 +7,7 @@ import numpy as np
 def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
     """Returns: the updated variable, the new first moment
     and the new second moment, respectively"""
-    s = beta1 * s + (1 - beta2) * np.square(grad)
+    s = beta2 * s + (1 - beta2) * np.square(grad)
     v = beta1 * v + (1 - beta1) * grad
     corr_v = v / (1 - beta1 ** t)
     corr_s = s / (1 - beta2 ** t)
