0b4daca6c1a4e2e4b59eb8ae45807a6e95132233
rania3103 <raniaabassi33@gmail.com>
2024-07-09 11:32:13 +0100

    Adam

---
+++ supervised_learning/optimization/9-Adam.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Write a function  that updates a variable in place
+using the Adam optimization algorithm"""
+import numpy as np
+
+
+def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
+    """Returns: the updated variable, the new first moment
+    and the new second moment, respectively"""
+    s = beta1 * s + (1 - beta2) * np.square(grad)
+    v = beta1 * v + (1 - beta1) * grad
+    corr_v = v / (1 - beta1 ** t)
+    corr_s = s / (1 - beta2 ** t)
+    var = var - alpha * corr_v / (np.sqrt(corr_s) + epsilon)
+    return var, v, s
