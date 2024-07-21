0b4fa916c4f74106cae8724cbb78c59c952e2bfd
rania3103 <raniaabassi33@gmail.com>
2024-07-21 16:55:38 +0100

    Initialize Normal

---
+++ math/probability/normal.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""a class Normal that represents a Normal distribution"""
+
+
+class Normal:
+    """Normal class"""
+
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """constructor"""
+        if data is None:
+            if stddev <= 0:
+                raise ValueError('stddev must be a positive value')
+            self.stddev = float(stddev)
+            self.mean = float(mean)
+        else:
+            if not isinstance(data, list):
+                raise TypeError('data must be a list')
+            if len(data) < 2:
+                raise ValueError('data must contain multiple values')
+            self.mean = sum(data) / len(data)
+            var = sum((numb - self.mean) ** 2 for numb in data) / len(data)
+            self.stddev = var ** 0.5
+            self.data = data
