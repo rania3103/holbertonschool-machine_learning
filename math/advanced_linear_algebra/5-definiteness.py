5e53c731a5b4f6f3822e8911591315d3be467bf7
rania3103 <raniaabassi33@gmail.com>
2024-07-21 14:48:18 +0100

    handle the case matrix is symetric

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -13,16 +13,17 @@ def definiteness(matrix):
     If matrix does not fit any of the above categories, return None"""
     if not isinstance(matrix, np.ndarray):
         raise TypeError('matrix must be a numpy.ndarray')
-    if matrix.size == 0 or matrix.shape[0] != matrix.shape[1]:
+    if matrix.size == 0 or matrix.shape[0] != matrix.shape[1] or \
+            not np.allclose(matrix.T, matrix):
         return None
     eigenvalues = np.linalg.eigvals(matrix)
     if np.all(eigenvalues > 0):
         return 'Positive definite'
     elif np.all(eigenvalues >= 0):
         return 'Positive semi-definite'
-    elif np.all(eigenvalues <= 0):
-        return 'Negative semi-definite'
     elif np.all(eigenvalues < 0):
         return 'Negative definite'
+    elif np.all(eigenvalues <= 0):
+        return 'Negative semi-definite'
     else:
         return 'Indefinite'
