c4bc235bcbb31d38aaaa6b68d2c0be6ef390c57e
rania3103 <raniaabassi33@gmail.com>
2024-07-22 11:17:32 +0100

    Posterior

---
+++ math/bayesian_prob/3-posterior.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+"""a function that calculates the likelihood of
+obtaining this data given various hypothetical probabilities
+of developing severe side effects"""
+import numpy as np
+
+
+def likelihood(x, n, P):
+    """Returns: a 1D numpy.ndarray containing the likelihood
+    of obtaining the data, x and n, for each probability
+    in P, respectively"""
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError('n must be a positive integer')
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(
+            'x must be an integer that is greater than or equal to 0')
+    if x > n:
+        raise ValueError('x cannot be greater than n')
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError('P must be a 1D numpy.ndarray')
+    if np.any(P > 1) or np.any(P < 0):
+        raise ValueError('All values in P must be in the range [0, 1]')
+    likelihood = np.zeros(P.shape)
+    for i, p in enumerate(P):
+        likelihood[i] = np.math.factorial(
+            n) / (np.math.factorial(x) * np.math.factorial(n - x)) \
+            * (p ** x) * ((1 - p) ** (n - x))
+    return likelihood
+
+
+def intersection(x, n, P, Pr):
+    """Returns: a 1D numpy.ndarray containing the intersection
+    of obtaining x and n with each probability in P, respectively"""
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError('n must be a positive integer')
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(
+            'x must be an integer that is greater than or equal to 0')
+    if x > n:
+        raise ValueError('x cannot be greater than n')
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError('P must be a 1D numpy.ndarray')
+    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
+        raise TypeError('Pr must be a numpy.ndarray with the same shape as P')
+    if np.any(P > 1) or np.any(P < 0):
+        raise ValueError('All values in P must be in the range [0, 1]')
+    if np.any(Pr > 1) or np.any(Pr < 0):
+        raise ValueError('All values in Pr must be in the range [0, 1]')
+    if not np.isclose(np.sum(Pr), 1):
+        raise ValueError('Pr must sum to 1')
+    likeli = likelihood(x, n, P)
+    return likeli * Pr
+
+
+def marginal(x, n, P, Pr):
+    """Returns: the marginal probability of obtaining x and n"""
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError('n must be a positive integer')
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(
+            'x must be an integer that is greater than or equal to 0')
+    if x > n:
+        raise ValueError('x cannot be greater than n')
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError('P must be a 1D numpy.ndarray')
+    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
+        raise TypeError('Pr must be a numpy.ndarray with the same shape as P')
+    if np.any(P > 1) or np.any(P < 0):
+        raise ValueError('All values in P must be in the range [0, 1]')
+    if np.any(Pr > 1) or np.any(Pr < 0):
+        raise ValueError('All values in Pr must be in the range [0, 1]')
+    if not np.isclose(np.sum(Pr), 1):
+        raise ValueError('Pr must sum to 1')
+    return np.sum(intersection(x, n, P, Pr))
+
+
+def posterior(x, n, P, Pr):
+    """Returns: the posterior probability of each probability
+    in P given x and n, respectively"""
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError('n must be a positive integer')
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(
+            'x must be an integer that is greater than or equal to 0')
+    if x > n:
+        raise ValueError('x cannot be greater than n')
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError('P must be a 1D numpy.ndarray')
+    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
+        raise TypeError('Pr must be a numpy.ndarray with the same shape as P')
+    if np.any(P > 1) or np.any(P < 0):
+        raise ValueError('All values in P must be in the range [0, 1]')
+    if np.any(Pr > 1) or np.any(Pr < 0):
+        raise ValueError('All values in Pr must be in the range [0, 1]')
+    if not np.isclose(np.sum(Pr), 1):
+        raise ValueError('Pr must sum to 1')
+    return likelihood(x, n, P) * Pr / marginal(x, n, P, Pr)
