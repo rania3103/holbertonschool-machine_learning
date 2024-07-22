622f5bb0a3ea826154d2af6966364b4d07af2818
rania3103 <raniaabassi33@gmail.com>
2024-07-22 11:00:47 +0100

    fix some cases

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -40,10 +40,12 @@ def intersection(x, n, P, Pr):
         raise ValueError('x cannot be greater than n')
     if not isinstance(P, np.ndarray) or P.ndim != 1:
         raise TypeError('P must be a 1D numpy.ndarray')
-    if not isinstance(P, np.ndarray) or P.shape != Pr.shape:
+    if not isinstance(P, np.ndarray) or Pr.shape != P.shape:
         raise TypeError('Pr must be a numpy.ndarray with the same shape as P')
-    if (np.any(P > 1) or np.any(P < 0)) or (np.any(Pr > 1) or np.any(Pr < 0)):
+    if (np.any(P > 1) or np.any(P < 0)):
         raise ValueError('All values in {P} must be in the range [0, 1]')
+    if (np.any(Pr > 1) or np.any(Pr < 0)):
+        raise ValueError('All values in {Pr} must be in the range [0, 1]')
     if not np.isclose(np.sum(Pr), 1):
         raise ValueError('Pr must sum to 1')
     likeli = likelihood(x, n, P)
