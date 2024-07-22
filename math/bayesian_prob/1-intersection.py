332404021ba959aaf50d5b9ed52b00d744f431e2
rania3103 <raniaabassi33@gmail.com>
2024-07-22 10:57:52 +0100

    fix it

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -1,7 +1,32 @@
 #!/usr/bin/env python3
-"""a function that calculates the  intersection of obtaining
-this data with the various hypothetical probabilities:"""
+"""a function that calculates the likelihood of
+obtaining this data given various hypothetical probabilities
+of developing severe side effects"""
 import numpy as np
+
+
+def likelihood(x, n, P):
+    """Returns: a 1D numpy.ndarray containing the likelihood
+    of obtaining the data, x and n, for each probability
+    in P, respectively"""
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError('n must be a positive integer')
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(
+            'x must be an integer that is greater than or equal to 0')
+    if x > n:
+        raise ValueError('x cannot be greater than n')
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError('P must be a 1D numpy.ndarray')
+    if np.any(P > 1) or np.any(P < 0):
+        raise ValueError('All values in P must be in the range [0, 1]')
+    likelihood = np.zeros(P.shape)
+    for i, p in enumerate(P):
+        likelihood[i] = np.math.factorial(
+            n) / (np.math.factorial(x) * np.math.factorial(n - x)) \
+            * (p ** x) * ((1 - p) ** (n - x))
+    return likelihood
+
 
 def intersection(x, n, P, Pr):
     """Returns: a 1D numpy.ndarray containing the intersection
@@ -21,6 +46,5 @@ def intersection(x, n, P, Pr):
         raise ValueError('All values in {P} must be in the range [0, 1]')
     if not np.isclose(np.sum(Pr), 1):
         raise ValueError('Pr must sum to 1')
-    likelihood = __import__('0-likelihood').likelihood
     likeli = likelihood(x, n, P)
     return likeli * Pr
