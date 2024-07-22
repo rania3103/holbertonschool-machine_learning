949b0c6b26da7560bee728b10de729737e29a032
rania3103 <raniaabassi33@gmail.com>
2024-07-22 12:06:08 +0100

    try to fix the error

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -7,9 +7,10 @@ def mean_cov(X):
     """Returns: mean, cov"""
     if not isinstance(X, np.ndarray) or X.ndim != 2:
         raise TypeError('X must be a 2D numpy.ndarray')
-    if X.ndim < 2:
-        raise ValueError('X must contain multiple data points')
     n = X.shape[0]
+    if n < 2:
+        raise ValueError('X must contain multiple data points')
+
     mean = np.mean(X, axis=0)
     cov = np.dot((X - mean).T, X - mean) / (n - 1)
     return mean, cov
