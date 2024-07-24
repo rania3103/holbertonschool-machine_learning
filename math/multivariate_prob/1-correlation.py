dd570e157224343b235773263601dd72171d388b
rania3103 <raniaabassi33@gmail.com>
2024-07-24 09:21:29 +0100

    ensure that C is a 2d square matrix

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -8,7 +8,7 @@ def correlation(C):
     containing the correlation matrix"""
     if not isinstance(C, np.ndarray):
         raise TypeError('C must be a numpy.ndarray')
-    if C.shape[0] != C.shape[1] or C.ndim != 2:
+    if C.ndim != 2 or C.shape[0] != C.shape[1]:
         raise ValueError('C must be a 2D square matrix')
     std_dev = np.sqrt(np.diag(C))
     d = C.shape[0]
