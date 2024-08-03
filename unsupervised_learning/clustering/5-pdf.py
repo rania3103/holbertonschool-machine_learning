25881a9728df5982195bf12a5b9f6ec17373df5f
rania3103 <raniaabassi33@gmail.com>
2024-08-03 11:58:50 +0100

    PDF

---
+++ unsupervised_learning/clustering/5-pdf.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""a function that calculates the probability density
+function of a Gaussian distribution"""
+import numpy as np
+
+
+def pdf(X, m, S):
+    """Returns: P(is a numpy.ndarray of shape (n,)
+    containing the PDF values for each data point),
+    or None on failure"""
+    n, d = X.shape
+    det = np.linalg.det(S)
+    norm_fact = 1 / ((2 * np.pi) ** (d / 2) * np.sqrt(det))
+    diff_X_m = X - m
+    inv = np.linalg.inv(S)
+    exp_fact = np.exp(-0.5 * np.sum(np.dot(diff_X_m, inv) * diff_X_m, axis=1))
+    P = norm_fact * exp_fact
+    return np.maximum(P, 1e-300)
