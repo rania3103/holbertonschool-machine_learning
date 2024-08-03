85f81e371fa298b0a9178d14f5e688a6850c6548
rania3103 <raniaabassi33@gmail.com>
2024-08-03 12:22:25 +0100

    Hello, sklearn!

---
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""a function that  performs K-means on a dataset"""
+from sklearn.cluster import KMeans
+
+
+def kmeans(X, k):
+    """C( of shape (k, d) containing the centroid
+    means for each cluster), clss(of shape (n,)
+    containing the index of the cluster in C that
+    each data point belongs to)"""
+    kmeans_ = KMeans(n_clusters=k)
+    kmeans_.fit(X)
+    C = kmeans_.cluster_centers_
+    clss = kmeans_.labels_
+    return C, clss
