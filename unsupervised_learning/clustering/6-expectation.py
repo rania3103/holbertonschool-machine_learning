21b37ffe503d68cc5504cdd53863ec5c0b3b6b3a
rania3103 <raniaabassi33@gmail.com>
2024-08-03 13:01:16 +0100

    Expectation

--- unsupervised_learning/clustering/6-expectation.py
+++ unsupervised_learning/clustering/6-expectation.py
@@ -2,30 +2,7 @@
 """a function that calculates the probability density
 function of a Gaussian distribution"""
 import numpy as np
-
-
-def pdf(X, m, S):
-    """Returns: P(is a numpy.ndarray of shape (n,)
-    containing the PDF values for each data point),
-    or None on failure"""
-    if not isinstance(X, np.ndarray) or X.ndim != 2:
-        return None
-    n, d = X.shape
-    if not isinstance(
-            m,
-            np.ndarray) or not isinstance(
-                S,
-            np.ndarray) or m.shape[0] != d or S.shape[0] != d\
-            or S.shape[1] != d:
-        return None
-
-    det = np.linalg.det(S)
-    norm_fact = 1 / ((2 * np.pi) ** (d / 2) * np.sqrt(det))
-    diff_X_m = X - m
-    inv = np.linalg.inv(S)
-    exp_fact = np.exp(-0.5 * np.sum(np.dot(diff_X_m, inv) * diff_X_m, axis=1))
-    P = norm_fact * exp_fact
-    return np.maximum(P, 1e-300)
+pdf = __import__('5-pdf').pdf
 
 
 def expectation(X, pi, m, S):
