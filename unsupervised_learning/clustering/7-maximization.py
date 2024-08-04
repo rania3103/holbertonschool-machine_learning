755edb8a604d5a964586d4009eacc268703e6d2f
rania3103 <raniaabassi33@gmail.com>
2024-08-04 11:43:02 +0100

    check g

--- unsupervised_learning/clustering/7-maximization.py
+++ unsupervised_learning/clustering/7-maximization.py
@@ -8,6 +8,8 @@ def maximization(X, g):
     """Returns: pi, m, S, or None, None, None on failure"""
     try:
         n, d = X.shape
+        if not isinstance(g, np.ndarray) or g.ndim != 2 or g.shape[1] != n:
+            return None, None, None
         k, n = g.shape
         pi = np.sum(g, axis=1) / n
         m = np.dot(g, X) / np.sum(g, axis=1)[:, None]
