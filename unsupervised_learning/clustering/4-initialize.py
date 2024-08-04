09ec53ebb8feab279622499772beb1702d6e5d9a
rania3103 <raniaabassi33@gmail.com>
2024-08-04 11:38:50 +0100

    check k

--- unsupervised_learning/clustering/4-initialize.py
+++ unsupervised_learning/clustering/4-initialize.py
@@ -7,6 +7,8 @@ kmeans = __import__('1-kmeans').kmeans
 def initialize(X, k):
     """Returns: pi, m, S, or None, None, None on failure"""
     try:
+        if not isinstance(k, int) or k <= 0:
+            return None, None, None
         n, d = X.shape
         pi = np.ones(k) / k
         C, clss = kmeans(X, k)
