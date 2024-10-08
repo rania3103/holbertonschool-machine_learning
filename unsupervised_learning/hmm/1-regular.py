0bb873a7587d36b9bc962738a48bd403dd2fecc6
rania3103 <raniaabassi33@gmail.com>
2024-10-08 11:25:18 +0100

    update docstring

--- unsupervised_learning/hmm/1-regular.py
+++ unsupervised_learning/hmm/1-regular.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""that determines the steady state probabilities of a regular markov chain"""
+"""a function that determines the steady state probabilities of a regular markov chain"""
 import numpy as np
 
 
