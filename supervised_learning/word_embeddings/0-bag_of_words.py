409498344aa2a3129cf8d98dd1262ba20a649d47
rania3103 <raniaabassi33@gmail.com>
2024-10-11 16:24:00 +0100

    fix case when vocab is none

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -6,9 +6,11 @@ import re
 
 def bag_of_words(sentences, vocab=None):
     """Returns: embeddings, features"""
-    tokens = [sentence.lower().split() for sentence in sentences]
-    tokens = [[re.sub(r"[^\w]", "", word) for word in sentence if re.match(
-        r"^\w+$", word)] for sentence in tokens]
+    tokens = []
+    for sentence in sentences:
+        cleaned_sentence = re.sub(r"'s\b", '', sentence.lower())
+        words = re.findall(r'\b\w+\b', cleaned_sentence)
+        tokens.append(words)
     if vocab is None:
         vocab = set()
         for sentence in tokens:
