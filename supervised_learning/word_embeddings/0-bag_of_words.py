2be84031abe5010f10291768c34ced3e0787d1ea
rania3103 <raniaabassi33@gmail.com>
2024-10-11 15:53:40 +0100

    fix case when vocab is none

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
 """a function that creates a bag of words embedding matrix"""
 import numpy as np
+import re
 
 
 def bag_of_words(sentences, vocab=None):
     """Returns: embeddings, features"""
     tokens = [sentence.lower().split() for sentence in sentences]
+    tokens = [[re.sub(r"[^\w]", "", word) for word in sentence if re.match(
+        r"^\w+$", word)] for sentence in tokens]
     if vocab is None:
-        vocab = []
+        vocab = set()
         for sentence in tokens:
             for word in sentence:
-                vocab.append(word)
-        vocab = sorted(set(vocab))
-    embeddings = np.zeros((len(sentences), len(vocab)))
+                vocab.add(word)
+        vocab = sorted(vocab)
+    embeddings = np.zeros((len(sentences), len(vocab)), dtype=int)
     for i, sentence in enumerate(tokens):
         for word in sentence:
-            if word in set(vocab):
+            if word in vocab:
                 embeddings[i][vocab.index(word)] += 1
     return embeddings, vocab
