baa5eecec189b7d71c20874addf372bccdb6f4d4
rania3103 <raniaabassi33@gmail.com>
2024-10-11 15:01:19 +0100

    Bag Of Words

---
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""a function that creates a bag of words embedding matrix"""
+import numpy as np
+
+
+def bag_of_words(sentences, vocab=None):
+    """Returns: embeddings, features"""
+    tokens = [sentence.lower().split() for sentence in sentences]
+    if vocab is None:
+        vocab = []
+        for sentnece in tokens:
+            for word in sentnece:
+                vocab.append(word)
+    vocab = sorted(set(vocab))
+    embeddings = np.zeros((len(sentences), len(vocab)))
+    for i, sentence in enumerate(tokens):
+        for word in sentence:
+            if word in vocab:
+                embeddings[i][vocab.index(word)] += 1
+    return embeddings, vocab
