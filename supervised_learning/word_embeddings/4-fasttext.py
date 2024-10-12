e86bf9029de787054cd793efb7c8087e404ea158
rania3103 <raniaabassi33@gmail.com>
2024-10-12 13:33:22 +0100

    FastText

---
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""a function that creates , builds and trains a gensim fastText model"""
+import gensim
+
+
+def fasttext_model(sentences, vector_size=100, min_count=5, negative=5,
+                   window=5, cbow=True, epochs=5, seed=0, workers=1):
+    """Returns: the trained model"""
+    sg = 1 if not cbow else 0
+    model = gensim.models.FastText(
+        sentences,
+        vector_size=vector_size,
+        min_count=min_count,
+        window=window,
+        negative=negative,
+        sg=sg,
+        epochs=epochs,
+        seed=seed,
+        workers=workers)
+    model.build_vocab(sentences)
+    model.train(
+        sentences,
+        total_examples=model.corpus_count,
+        epochs=model.epochs)
+    return model
