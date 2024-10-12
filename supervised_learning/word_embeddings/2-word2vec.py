9bd9841c2e184935f00725b986a33f415fee4864
rania3103 <raniaabassi33@gmail.com>
2024-10-12 12:19:13 +0100

    Train Word2Vec

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """a function that creates , builds and trains a gensim word2vec model"""
-from gensim.models import Word2Vec
+import gensim
 
 
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
@@ -10,7 +10,7 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
         sg = 1
     else:
         sg = 0
-    model = Word2Vec(
+    model = gensim.models.Word2Vec(
         sentences,
         vector_size=vector_size,
         min_count=min_count,
