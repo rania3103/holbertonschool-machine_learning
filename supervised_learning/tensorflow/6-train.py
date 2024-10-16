e69cce91202eeca0c8badd79afc93ef25a71cbdd
rania3103 <raniaabassi33@gmail.com>
2024-10-16 08:54:19 +0100

    trying to fix it

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -22,14 +22,14 @@ def train(
     """ Returns: the path where the model was saved"""
     tf.disable_eager_execution()
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
-    y_hat = forward_prop(x, layer_sizes, activations)
-    accuracy = calculate_accuracy(y, y_hat)
-    loss = calculate_loss(y, y_hat)
+    y_pred = forward_prop(x, layer_sizes, activations)
+    accuracy = calculate_accuracy(y, y_pred)
+    loss = calculate_loss(y, y_pred)
     train_op = create_train_op(loss, alpha)
 
     tf.add_to_collection("x", x)
     tf.add_to_collection("y", y)
-    tf.add_to_collection("y_pred", y_hat)
+    tf.add_to_collection("y_pred", y_pred)
     tf.add_to_collection("loss", loss)
     tf.add_to_collection("accuracy", accuracy)
     tf.add_to_collection("train_op", train_op)
