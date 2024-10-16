860f3f2f13bc0ceb6b71d89dbb9b4b47e54b5c65
rania3103 <raniaabassi33@gmail.com>
2024-10-16 14:23:09 +0100

    add  check cases

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -7,11 +7,16 @@ variance = __import__('2-variance').variance
 
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     """results, d_vars, or None, None on failure"""
-
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
+        return None, None
+    if not isinstance(kmin, int) or kmin <= 0:
+        return None, None
+    if not isinstance(kmax, int) or kmax < kmin:
+        return None, None
+    if not isinstance(iterations, int) or iterations <= 0:
+        return None, None
     if kmax is None:
         kmax = X.shape[0]
-    if kmin < 1 or kmax < kmin:
-        return None, None
     results = []
     d_vars = []
     C, clss = kmeans(X, kmin, iterations)
