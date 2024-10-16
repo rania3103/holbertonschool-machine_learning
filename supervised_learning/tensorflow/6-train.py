f6a5c81f440baaf7843e1a0d0e489fe4c8a3e59e
rania3103 <raniaabassi33@gmail.com>
2024-10-16 08:11:36 +0100

    Train

---
+++ supervised_learning/tensorflow/6-train.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""Write a function that builds, trains
+and saves a neural network classifier"""
+import tensorflow.compat.v1 as tf
+calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
+calculate_loss = __import__('4-calculate_loss').calculate_loss
+create_placeholders = __import__('0-create_placeholders').create_placeholders
+create_train_op = __import__('5-create_train_op').create_train_op
+forward_prop = __import__('2-forward_prop').forward_prop
+
+
+def train(
+        X_train,
+        Y_train,
+        X_valid,
+        Y_valid,
+        layer_sizes,
+        activations,
+        alpha,
+        iterations,
+        save_path="/tmp/model.ckpt"):
+    """ Returns: the path where the model was saved"""
+    x, y = create_placeholders(X_train, Y_train)
+    y_hat = forward_prop(X_train, layer_sizes, activations)
+    loss = calculate_loss(Y_train, y_hat)
+    accuracy = calculate_accuracy(y, y_hat)
+    train_op = create_train_op(loss, alpha)
+
+    tf.add_to_collection("x", x)
+    tf.add_to_collection("y", y)
+    tf.add_to_collection("y_hat", y_hat)
+    tf.add_to_collection("loss", loss)
+    tf.add_to_collection("accuracy", accuracy)
+    tf.add_to_collection("train_op", train)
+    init = tf.global_variables_initializer()
+    saver = tf.train.saver()
+    with tf.Session() as sess:
+        sess.run(init)
+    for i in range(iterations + 1):
+        sess.run(train_op, feed_dict={x: X_train, y: Y_train})
+        train_loss, train_accuracy = sess.run(
+            [loss, accuracy], feed_dict={x: X_train, y: Y_train})
+        valid_loss, valid_accuracy = sess.run(
+            [loss, accuracy], feed_dict={x: X_valid, y: Y_valid})
+        if i % 100 == 0 or i == iterations:
+            print(f"After {i} iterations:")
+            print(f"\tTraining Cost: {train_loss}")
+            print(f"\tTraining Accuracy: {train_accuracy}")
+            print(f"\tValidation Cost: {valid_loss}")
+            print(f"\tValidation Accuracy: {valid_accuracy}")
+        save_path = saver.save(sess, save_path)
+    return save_path
