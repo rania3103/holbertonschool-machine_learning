7258e677779ee6540af14f6b1bf0340c7c0c0a5c
rania3103 <raniaabassi33@gmail.com>
2024-10-21 10:17:56 +0100

    fix it

--- supervised_learning/transformer_apps/4-create_masks.py
+++ supervised_learning/transformer_apps/4-create_masks.py
@@ -11,10 +11,10 @@ def create_masks(inputs, target):
         return mask[:, tf.newaxis, tf.newaxis, :]
 
     encoder_mask = padding_mask(inputs)
-    decoder_mask = padding_mask(target)
+    decoder_padding_mask = padding_mask(target)
     seq_len = tf.shape(target)[1]
     look_ahead_mask = 1 - \
         tf.linalg.band_part(tf.ones((seq_len, seq_len)), -1, 0)
-    look_ahead_mask = look_ahead_mask[tf.newaxis, tf.newaxis, :, :]
-    combined_mask = tf.maximum(decoder_mask, look_ahead_mask)
+    combined_mask = tf.maximum(decoder_padding_mask, look_ahead_mask)
+    decoder_mask = padding_mask(inputs)
     return encoder_mask, combined_mask, decoder_mask
