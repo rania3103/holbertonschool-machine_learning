6cd6eaca3aa5c55f42f5ead6e919e968c04cb0ba
rania3103 <raniaabassi33@gmail.com>
2024-10-22 18:24:15 +0100

    Play

--- reinforcement_learning/q_learning/4-play.py
+++ reinforcement_learning/q_learning/4-play.py
@@ -10,13 +10,13 @@ def play(env, Q, max_steps=100):
     state, _ = env.reset()
     total_rewards = 0
     rendered_outputs = []
-    rendered_outputs.append(env.render())
     for step in range(max_steps):
+        rendered_outputs.append(env.render())
         action = np.argmax(Q[state])
         next_state, reward, done, truncated, info = env.step(action)
         total_rewards += reward
-        rendered_outputs.append(env.render())
         if done:
             break
         state = next_state
+    rendered_outputs.append(env.render())
     return total_rewards, rendered_outputs
