ee0ddcacfbadee93555e4b230a9f6b23afd851d3
rania3103 <raniaabassi33@gmail.com>
2024-10-23 07:36:46 +0100

    add render_mode=ansi

--- reinforcement_learning/q_learning/0-load_env.py
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -10,4 +10,5 @@ def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
         'FrozenLake-v1',
         desc=desc,
         map_name=map_name,
-        is_slippery=is_slippery, render_mode="ansi")
+        is_slippery=is_slippery,
+        render_mode="ansi")
