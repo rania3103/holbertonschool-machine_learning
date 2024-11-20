c3fcb8771b4cdbc670acb3da01530b63244e95de
rania3103 <raniaabassi33@gmail.com>
2024-11-20 12:08:45 +0100

    Concat

---
+++ pipeline/pandas/11-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""a function  that takes two pd.DataFrame objects and
+Indexes both dataframes on their Timestamp columns,
+Includes all timestamps from df2 (bitstamp) up to and
+including timestamp 1417411920,
+Concatenates the selected rows from df2 to the top of df1 (coinbase) and
+Adds keys to the concatenated data, labeling the rows
+from df2 as bitstamp and the rows from df1 as coinbase"""
+import pandas as pd
+index = __import__('10-index').index
+
+
+def concat(df1, df2):
+    """Returns the concatenated pd.DataFrame"""
+    df1, df2 = index(df1), index(df2)
+    df2 = df2[df2.index <= 1417411920]
+    return pd.concat([df1, df2], keys=['bitstamp', 'coinbase'])
