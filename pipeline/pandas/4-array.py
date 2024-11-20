fe8eba28f3c2f9688f7e26c6a2fed49e1e63374a
rania3103 <raniaabassi33@gmail.com>
2024-11-20 07:59:20 +0100

    delete import

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -2,7 +2,6 @@
 """a function that takes a pd.DataFrame as input and
 select the last 10 rows of the High and Close columns
 then Convert these selected values into a numpy.ndarray."""
-import pandas as pd
 
 
 def array(df):
