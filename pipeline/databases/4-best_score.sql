fb8805fa03249db76a5b749a75d6e722a3dfbf17
rania3103 <raniaabassi33@gmail.com>
2024-11-26 09:10:24 +0100

    fix output

--- pipeline/databases/4-best_score.sql
+++ pipeline/databases/4-best_score.sql
@@ -1,3 +1,4 @@
 -- a script that lists all records with a score >= 10 in the table second_table in MySQL server.
-SELECT * FROM second_table
-WHERE score >= 10;
+SELECT score, name FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
