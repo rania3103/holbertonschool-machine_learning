4a0abff491d0d3c6c295b47df0031eeba501e7d5
rania3103 <raniaabassi33@gmail.com>
2024-11-27 08:23:29 +0100

    Change school topics

---
+++ pipeline/databases/32-update_topics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""a Python function that changes all topics
+of a school document based on the name"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """mongo_collection will be the pymongo collection objec
+    topics (list of strings)will be the list
+    of topics approached in the school"""
+    mongo_collection.updateMany(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
