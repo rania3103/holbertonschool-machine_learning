6695df3384240772a4a5a3ac25d50c49364eb305
rania3103 <raniaabassi33@gmail.com>
2024-11-27 09:32:28 +0100

    trying to fix it

--- pipeline/databases/21-div.sql
+++ pipeline/databases/21-div.sql
@@ -1,12 +1,15 @@
 -- a SQL script that creates a function SafeDiv that divides (and returns)
 --the first by the second number or returns 0 if the second number is equal to 0.
-DELIMITER $$
+DELIMITER //
+
 CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS INT
+RETURNS FLOAT
 BEGIN
-    RETURN CASE
-        WHEN b = 0 THEN 0
-        ELSE a / b
-    END;
-END$$
+    IF b = 0 THEN
+        RETURN 0;
+    ELSE
+        RETURN a / b;
+    END IF;
+END//
+
 DELIMITER ;
