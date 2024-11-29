e2ee03665ad6fb3444026f438a409033c11cb8bb
rania3103 <raniaabassi33@gmail.com>
2024-11-29 10:39:33 +0100

    trying to fix it

--- pipeline/databases/21-div.sql
+++ pipeline/databases/21-div.sql
@@ -2,7 +2,7 @@
 --the first by the second number or returns 0 if the second number is equal to 0.
 DELIMITER //
 CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS FLOAT
+RETURNS FLOAT DETERMINISTIC
 BEGIN
     IF b=0 THEN
         RETURN 0;
