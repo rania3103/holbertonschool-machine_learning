7a1b09d5375fb892407dec161971059ceabf5422
rania3103 <raniaabassi33@gmail.com>
2024-12-09 09:24:45 +0100

    fix pep8

--- pipeline/data_augmentation/0-flip.py
+++ pipeline/data_augmentation/0-flip.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """a function that flips an image horizontally"""
 import tensorflow as tf
+
+
 def flip_image(image):
     """Returns the flipped image"""
     return tf.image.flip_left_right(image)
